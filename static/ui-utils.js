ddbba9d190a064ce38edaaade5bede898a7a2931
Ryan Huang 黃晧倫 <ryan.huang@unitedorthopedic.com>
2026-01-29 10:36:10 +0800

    重構(前端)：整合 UI 共用工具與模板腳本，抽出表單提交/轉義共用函式，降低模板 JS 重複

---
+++ static/ui-utils.js
@@ -0,0 +1,34 @@
+﻿(function () {
+  "use strict";
+
+  function submitPostForm(action, fields) {
+    var form = document.createElement("form");
+    form.method = "post";
+    form.action = action;
+    var entries = fields ? Object.keys(fields) : [];
+    for (var i = 0; i < entries.length; i++) {
+      var name = entries[i];
+      var input = document.createElement("input");
+      input.type = "hidden";
+      input.name = name;
+      input.value = fields[name];
+      form.appendChild(input);
+    }
+    document.body.appendChild(form);
+    form.submit();
+  }
+
+  function escapeHtml(value) {
+    return String(value || "")
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#39;");
+  }
+
+  window.UiUtils = Object.assign(window.UiUtils || {}, {
+    submitPostForm: submitPostForm,
+    escapeHtml: escapeHtml
+  });
+})();
